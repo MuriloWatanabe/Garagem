40d9591ef5735abb2511b2f632003bda3b76c165
murilowatanabe <muriloenzowatanabe14@gmail.com>
2023-03-10 16:40:04 -0300

    Criação do modelo Categoria

--- garagem/models.py
+++ garagem/models.py
@@ -5,7 +5,7 @@ class Marca(models.Model):
  nacionalidade = models.CharField(max_length=50, null=True, blank=True)
 
  def _str_(self):
-  return self.nome,self
+  return self.nome
  
 class Categoria(models.Model):
  descricao =models.CharField(max_length=100)
