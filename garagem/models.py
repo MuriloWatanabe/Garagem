99a7fa8ab6f0d2272659cd5f7c90eb2b8540755c
murilowatanabe <muriloenzowatanabe14@gmail.com>
2023-03-14 13:59:33 -0300

    finalização

--- garagem/models.py
+++ garagem/models.py
@@ -1,14 +1,17 @@
 from django.db import models
 
-class Marca(models.Model):
- nome =models.CharField(max_length=50)
- nacionalidade = models.CharField(max_length=50, null=True, blank=True)
 
- def _str_(self):
-  return self.nome
- 
+class Marcas(models.Model):
+    nome = models.CharField(max_length=50)
+    nacionalidade = models.CharField(max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return self.nome.upper()
+
+
 class Categoria(models.Model):
- descricao =models.CharField(max_length=100)
+    descricao = models.CharField(max_length=100)
 
- def _str_(self):
-  return self.descricao 
+    def __str__(self):
+        return self.descricao
+
