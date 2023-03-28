75842ae2cd97fee759364cff88cf2f26c59ae642
murilowatanabe <muriloenzowatanabe14@gmail.com>
2023-03-28 14:31:35 -0300

    Criação Acessorio

--- garagem/admin.py
+++ garagem/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Marcas
+from .models import Marca
 from .models import Categoria
+from .models import Acessorio
 
-admin.site.register(Marcas)
+admin.site.register(Marca)
 admin.site.register(Categoria)
+admin.site.register(Acessorio)
--- garagem/models.py
+++ garagem/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Marcas(models.Model):
+class Marca(models.Model):
     nome = models.CharField(max_length=50)
     nacionalidade = models.CharField(max_length=50, null=True, blank=True)
 
@@ -10,6 +10,12 @@ class Marcas(models.Model):
 
 
 class Categoria(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
+    
+class Acessorio(models.Model):
     descricao = models.CharField(max_length=100)
 
     def __str__(self):
