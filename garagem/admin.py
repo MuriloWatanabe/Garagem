d473697c116d7dbd7eb9ed6f261ccde0aefce713
murilowatanabe <muriloenzowatanabe14@gmail.com>
2023-03-28 14:38:00 -0300

    Criacao Cor

--- garagem/admin.py
+++ garagem/admin.py
@@ -3,7 +3,9 @@ from django.contrib import admin
 from .models import Marca
 from .models import Categoria
 from .models import Acessorio
+from .models import Cor
 
 admin.site.register(Marca)
 admin.site.register(Categoria)
 admin.site.register(Acessorio)
+admin.site.register(Cor)
--- garagem/models.py
+++ garagem/models.py
@@ -21,3 +21,11 @@ class Acessorio(models.Model):
     def __str__(self):
         return self.descricao
 
+class Cor(models.Model):
+    descricao = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.descricao
+    
+    class Meta:
+        verbose_name_plural = 'Cores'
