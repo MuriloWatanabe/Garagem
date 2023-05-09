3cdd6c544a2442cb03a773cb83c1110c98cee69b
murilowatanabe <muriloenzowatanabe14@gmail.com>
2023-05-09 14:29:59 -0300

    criação chave estrangeira modelo

--- garagem/models.py
+++ garagem/models.py
@@ -44,7 +44,7 @@ class Veiculo(models.Model):
     cor = models.ForeignKey(Cor, on_delete=models.PROTECT, related_name="veiculo")
     ano = models.IntegerField(null=True, default=0)
     preco = models.DecimalField(max_digits=100, decimal_places=2, null=True, default=0)
-    modelo = models.CharField(max_length=50)
+    modelo = models.ForeignKey(Modelo, on_delete=models.PROTECT, related_name="veiculo")
 
     def __str__(self):
         return f"Modelo:{self.modelo} - Cor:{self.cor} - Ano:{self.ano} -  Marca:{self.marca}"
