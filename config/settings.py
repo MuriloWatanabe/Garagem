38402ddc5502d139d727f87ad4d0f579bf603578
murilowatanabe <muriloenzowatanabe14@gmail.com>
2023-08-22 13:25:03 -0300

    Aula13

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
+    # "rest_framework_simplejwt",
     "corsheaders",
     "garagem"
 ]
@@ -133,7 +134,7 @@ REST_FRAMEWORK = {
     # "DEFAULT_PERMISSION_CLASSES": [
     #     "rest_framework.permissions.IsAuthenticated",
     # ]
-    "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.DjangoModelPermissions",
-    ],
+#    "DEFAULT_AUTHENTICATION_CLASSES": (
+#         "rest_framework_simplejwt.authentication.JWTAuthentication",
+#     ),
 }
