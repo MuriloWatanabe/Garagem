4082e1df33015280b45b0dba82b770d1fd1bcdd7
murilowatanabe14 <muriloenzowatanabe14@gmail.com>
2023-08-25 14:29:51 -0300

    finalização A4 (ativiade)

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     "corsheaders",
     "uploader",
     "drf_spectacular",
+    "django_extensions",
     "usuario",
     "garagem"
 ]
--- garagem/models/modelo.py
+++ garagem/models/modelo.py
@@ -1,9 +1,13 @@
 from django.db import models
 from .marca import Marca
+from .categoria import Categoria
 class Modelo(models.Model):
     descricao = models.CharField(max_length=100)
     marca = models.ForeignKey(
         Marca , on_delete=models.PROTECT, related_name="modelo")
+    categoria = models.ForeignKey(Categoria, on_delete=models.PROTECT, related_name="veiculo")
+
+    
     
     def __str__(self):
-        return f"{self.descricao} | {self.marca}"   
+        return f"{self.descricao} | {self.marca} | {self.categoria}"   
